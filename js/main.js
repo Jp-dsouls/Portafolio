700e541f4d34d5faaa6c04f80a5a1eaeda1740aa
Junior Pezantes <werras04@gmail.com>
2023-01-03 18:16:23 -0500

    feat: view-habilidades

---
+++ js/main.js
@@ -0,0 +1,59 @@
+function cargaAnimada() {
+  $(function () {
+    $('.chart1').easyPieChart({
+      size: 160,
+      barColor: "#ff5b00",
+      scaleLength: 0,
+      lineWidth: 15,
+      tackColor: "#525151",
+      lineCap: "circle",
+      animate: 2000,
+    });
+    $('.chart2').easyPieChart({
+      size: 160,
+      barColor: "#ff9900",
+      scaleLength: 0,
+      lineWidth: 15,
+      tackColor: "#0748a5",
+      lineCap: "circle",
+      animate: 2000,
+    });
+    $('.chart3').easyPieChart({
+      size: 160,
+      barColor: "#cc3348",
+      scaleLength: 0,
+      lineWidth: 15,
+      tackColor: "#0748a5",
+      lineCap: "circle",
+      animate: 2000,
+    });
+    $('.chart4').easyPieChart({
+      size: 160,
+      barColor: "#8d8d8d",
+      scaleLength: 0,
+      lineWidth: 15,
+      tackColor: "#0748a5",
+      lineCap: "circle",
+      animate: 2000,
+    });
+    $('.chart5').easyPieChart({
+      size: 160,
+      barColor: "#00deef",
+      scaleLength: 0,
+      lineWidth: 15,
+      tackColor: "#0748a5",
+      lineCap: "circle",
+      animate: 2000,
+    });
+    $('.chart6').easyPieChart({
+      size: 160,
+      barColor: "#3e3e3e",
+      scaleLength: 0,
+      lineWidth: 15,
+      tackColor: "#0748a5",
+      lineCap: "circle",
+      animate: 2000,
+    });
+  })
+}
+cargaAnimada();
